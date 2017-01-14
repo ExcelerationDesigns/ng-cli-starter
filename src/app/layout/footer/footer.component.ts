44ea932f3ec9b3cfe71eb358ce915a53c76d4195
JT-Bruch <jt.bruch@crownpeak.com>
2017-01-13 17:03:31 -0700

    NS: Fix linting.

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html'
 })
 export class FooterComponent implements OnInit {
+  row: any;
 
   constructor() { }
 
